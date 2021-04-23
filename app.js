c40cad2158a0b712e65fe299a89f2c54083e4801
calvin0630 <cg7057705@gmail.com>
2021-04-22 23:48:35 -0400

    working on audiobook page

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+
 var express = require("express");
-var partials = require('express-partials');
 var path = require("path");
+var fs = require("fs");
+
 
 app = express();
 app.set("port", process.env.port || 3000);
@@ -9,7 +11,7 @@ app.set('view engine', 'ejs');
 // Include partials middleware into the server
 app.use(express.static(__dirname + '/public'));
 
-
+readAudiobookFiles()
 
 // use res.render to load up an ejs view file
 
@@ -32,3 +34,29 @@ app.get('/shader-demo', function(req, res) {
 app.listen(app.get("port"), function () {
     console.log("server started on port: " + app.get("port"));
 });
+
+function readAudiobookFiles() {
+
+    let files = fs.readdirSync(__dirname +'/public/audiobooks/');
+    let mp3Regex = /([a-zA-Z0-9\s_\\.\-\(\):])+(.mp3)$/;
+    for (f in files) {
+        //if its an mp3
+        if (mp3Regex.test(files[f])) {
+            console.log("mp3: "+ files[f]);
+        }
+        //otherwise assume its a directory
+        else {
+            console.log("folder: "+ files[f]);
+        }
+    }
+    console.log("full list");
+    console.log(files);
+}
+
+function Book() {
+    this.title;
+    this.filename
+    this.author;
+    this.directory;
+    this.oneFile
+}
